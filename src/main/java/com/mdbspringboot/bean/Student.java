06712cc9e4c26577b73a431476faf32b474c960e
EladAvrahami <86184072+EladAvrahami@users.noreply.github.com>
2022-08-08 11:39:18 +0300

    @PersistenceConstructor explanation

--- src/main/java/com/mdbspringboot/bean/Student.java
+++ src/main/java/com/mdbspringboot/bean/Student.java
@@ -1,9 +1,11 @@
 package com.mdbspringboot.bean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
@@ -22,5 +24,17 @@ public class Student {
     private Department department;
     private List<Subject> subjects;
 
+  /*  @PersistenceConstructor //in case u have more than one con tell mongo which of them be related to collection(only in the class which representing the collection)
+    public Student(String id, String name, String email, Department department, List<Subject> subjects) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.department = department;
+        this.subjects = subjects;
+    }
 
+    public Student(String id, Department department) {
+        this.id = id;
+        this.department = department;
+    }*/
 }
