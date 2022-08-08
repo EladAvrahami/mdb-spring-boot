fd976a11568a3ca3441dbb9640387fc02a3e7cc2
EladAvrahami <86184072+EladAvrahami@users.noreply.github.com>
2022-08-08 11:54:53 +0300

    @EnableMongoRepositories("com.mdbspringboot.repo") //need to provide the packages where our repositories are located (@Repository anno)

--- src/main/java/com/mdbspringboot/MdbSpringBootApplication.java
+++ src/main/java/com/mdbspringboot/MdbSpringBootApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
+@EnableMongoRepositories("com.mdbspringboot.repo") //need to provide the packages where our repositories are located
 public class MdbSpringBootApplication {
 
 
