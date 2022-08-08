b668d6282728bf7c5f99c86cfd5202dbc0258fb3
EladAvrahami <86184072+EladAvrahami@users.noreply.github.com>
2022-08-08 11:40:06 +0300

    add Department class - a sub document

--- src/main/java/com/mdbspringboot/bean/Department.java
+++ src/main/java/com/mdbspringboot/bean/Department.java
@@ -1,6 +1,16 @@
 package com.mdbspringboot.bean;
 
-public class Department {
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Getter
+@Setter
+public class Department {//is a sub document
+
+    @Field(name = "department_name")
+    private String departmentName;
+    private String location;
 
 
 }
